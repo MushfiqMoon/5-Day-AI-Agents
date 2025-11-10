bc33b1dab34a7611271a37e5c95ac1f0b8ab4581
mushfiqur.rahman@sjinnovation.com <mushfiqur.rahman@sjinnovation.com>
2025-11-11 01:37:00 +0600

    done

--- agent.py
+++ agent.py
@@ -1,14 +1,31 @@
 from google.adk.agents.llm_agent import Agent
 
-# Mock tool implementation
-def get_current_time(city: str) -> dict:
-    """Returns the current time in a specified city."""
-    return {"status": "success", "city": city, "time": "11:30 PM"}
+from google.adk.agents import Agent
+from google.adk.runners import InMemoryRunner
+from google.adk.tools import google_search
+from google.genai import types
+
+print("✅ ADK components imported successfully.")
+
+# # Mock tool implementation
+# def get_current_time(city: str) -> dict:
+#     """Returns the current time in a specified city."""
+#     return {"status": "success", "city": city, "time": "11:30 PM"}
+
+# root_agent = Agent(
+#     model='gemini-2.5-flash',
+#     name='root_agent',
+#     description="Tells the current time in a specified city.",
+#     instruction="You are a helpful assistant that tells the current time in cities. Use the 'get_current_time' tool for this purpose.",
+#     tools=[get_current_time],
+# )
 
 root_agent = Agent(
-    model='gemini-2.5-flash',
-    name='root_agent',
-    description="Tells the current time in a specified city.",
-    instruction="You are a helpful assistant that tells the current time in cities. Use the 'get_current_time' tool for this purpose.",
-    tools=[get_current_time],
+    name="helpful_assistant",
+    model="gemini-2.5-flash-lite",
+    description="A simple agent that can answer general questions.",
+    instruction="You are a helpful assistant. Use Google Search for current info or if unsure.",
+    tools=[google_search],
 )
+
+print("✅ Root Agent defined.")
